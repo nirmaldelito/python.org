7697637f95cc964a2da1293b7255a3e3f115ab90
nirmaldelito <42384832+nirmaldelito@users.noreply.github.com>
2018-08-18 19:27:02 +0530

    Update and rename trail.py to functions.py

--- functions.py
+++ functions.py
@@ -3,16 +3,16 @@
 def num(x,a,b):
     if x is "add":
         z = a + b
-        return(z)
+      
     elif x is "sub":
         z = a - b
-        return(z)
+        
     elif x is "div":
         z = a / b
-        return(z)
+        
     elif x is "mul":
         z = a * b
-        return(z)
+    return(z)
 
 pop = num("add",12,12)
 
